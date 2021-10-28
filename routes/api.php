b196eeaf81d14fc60212874dd75f0a257dbe8156
fs2050 <fabiolfs76@hotmail.com>
2021-10-28 02:44:11 -0300

    initial api

--- routes/api.php
+++ routes/api.php
@@ -15,11 +15,11 @@ use App\Http\Controllers\PostController;
 |
 */
 
-Route::get('testeing', function(){
+Route::get('api/testeing', function(){
     return "hello world";
 });
 
-Route::resource('post', PostController::class);
+Route::resource('api/post', PostController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
