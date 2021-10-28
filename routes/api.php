b4c4570191817efebd2d30ae39c3177d5d51df44
fs2050 <fabiolfs76@hotmail.com>
2021-10-28 01:43:32 -0300

    initial api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('testeing', function(){
+    return "hello world";
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
