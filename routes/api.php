f46bcba6d3184cfa4fc4302e22aa536fb03922eb
fs2050 <fabiolfs76@hotmail.com>
2021-10-29 20:44:50 -0300

    release initial

--- routes/api.php
+++ routes/api.php
@@ -16,15 +16,15 @@ use App\Http\Controllers\AuthController;
 |
 */
 
-Route::post('/register',[AuthController::class, 'register'] );
-Route::post('/login',[AuthController::class, 'login'] )->name("login");
+Route::post('register',[AuthController::class, 'register'] );
+Route::post('login',[AuthController::class, 'login'] )->name("login");
 
 
 
 
 
 Route::middleware('auth:sanctum')->group( function () {
-    Route::resource('/post', PostController::class)->except([
+    Route::resource('post', PostController::class)->except([
         'create', 'edit'
     ]);
 
