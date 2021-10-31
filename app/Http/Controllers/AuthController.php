49335bf34dabf8882cc334ce440b77717be39c7e
fs2050 <fabiolfs76@hotmail.com>
2021-10-31 20:04:23 -0300

    initial release

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,7 +19,7 @@ class AuthController extends Controller
                 'name' => 'required',
                 'email' => 'required|email',
                 'password' => 'required',
-                'password2' => 'required|same:password',
+                //'password2' => 'required|same:password',
 
             ]
         );
@@ -54,7 +54,7 @@ class AuthController extends Controller
         $validator = Validator::make(
             $input,
             [
-    
+
                 'email' => 'required|email',
                 'password' => 'required',
 
