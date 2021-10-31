cdb0adcaea459c7f1e9008963354fdf3edf832e4
fs2050 <fabiolfs76@hotmail.com>
2021-10-31 19:32:50 -0300

    initial release

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,10 +18,11 @@ class User extends Authenticatable
      * @var string[]
      */
     protected $fillable = [
-    
+         'id',
         'name',
         'email',
-        'password'
+        'password',
+        'password2'
     ];
 
     /**
