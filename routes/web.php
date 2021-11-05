b7bdddbec693cce071c07234f25e1ae53534aaec
fs2050 <fabiolfs76@hotmail.com>
2021-11-05 04:00:29 -0300

    deploy

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/register',[AuthController::class, 'register'] )->name("register");
+Route::get('/login',[AuthController::class, 'login'] )->name("login");
+
+Route::resource('get', PostController::class)->except([
+    'create', 'edit'
+]);
+
+
+
+Route::middleware('auth:sanctum')->group( function () {
+   /*  Route::resource('/post', PostController::class)->except([
+        'create', 'edit'
+    ]);
+ */
+});
