d10f5829862be00d9e832b07d876655e2df1eccd
fs2050 <fabiolfs76@hotmail.com>
2021-11-07 19:12:13 -0300

    deploy

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -104,6 +104,9 @@
                 1° campo-> Key: Accept e Value: application/json<br>
                 2° campo-> Key: Authorization e Value: Bearer + o Token recebido na hora do registro.
             </p>
+               <p>No Body insira seu email e sua password</p><br>
+               "email":"email123@email",<br>
+               "password":"sua senha"
             <hr>
             <p class="lead">Rota de Inserção dos dados ->
                 Verbo: Post
